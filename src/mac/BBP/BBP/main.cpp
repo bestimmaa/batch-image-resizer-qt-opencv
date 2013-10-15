0a4185a1cb1bef175eb0d99682e53c2750f9e5e3
Christoph Halang <christoph.halang@gmail.com>
2013-10-15 16:45:03 +0200

    building for brew installation on os x

---
+++ src/mac/BBP/BBP/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  BBP
+//
+//  Created by Christoph Halang on 15.10.13.
+//  Copyright (c) 2013 Christoph Halang. All rights reserved.
+//
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
